4c4be49bc961521c003f86e625367864341e9de9
count-stars-7 <count-stars@outlook.com>
2026-05-05 22:37:02 +0800

    1.1添加注释

--- CCF_CSP/1.cpp
+++ CCF_CSP/1.cpp
@@ -1,6 +1,6 @@
 ﻿#define  _CRT_SECURE_NO_WARNINGS
 //===============1.1相反数========================
-
+//数字不重复，时间复杂度n，空间复杂度m
 #include<stdio.h>	
 int nums[1010]; int cnt[1010];
 int main() {
@@ -12,7 +12,22 @@ int main() {
 	int ans = 0;
 	for (int i = 0; i < n; i++) {
 		if (nums[i]< 0)		ans += cnt[-nums[i]];
+		//当num<0他的相反数-num如果出现过那么cnt[num]=1则ans+=1，若没出现过ans+=0
 	}
 	printf("%d", ans);
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
