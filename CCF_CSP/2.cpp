49bf5b95c34a6b5bac3c7e749c87255587b8ee3c
count-stars-7 <count-stars@outlook.com>
2026-05-10 14:06:05 +0800

    2.1相邻对数

---
+++ CCF_CSP/2.cpp
@@ -0,0 +1,22 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+
+//==============2.1相邻对数==============
+//#include<stdio.h>
+//int main() {
+//	int n; scanf("%d", &n);
+//	int num;
+//	int vis[1010] = { 0 };
+//	for (int i = 1; i <= n; i++) {
+//		scanf("%d", &num);
+//		vis[num] = 1;
+//	}
+//	int ans = 0;
+//	for (int i = 1; i <= 1010-1; i++) {
+//		if (vis[i] == 1 && vis[i + 1] == 1) {
+//			ans++;
+//		}
+//	}
+//	printf("%d", ans);
+//	return 0;
+//}
+////标记数组法，比较排序和枚举更优
