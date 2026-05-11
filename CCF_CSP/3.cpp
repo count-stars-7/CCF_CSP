b9e97ee39d3e4628037489476e577165f5fef657
count-stars-7 <count-stars@outlook.com>
2026-05-11 23:30:23 +0800

    3.1门禁系统

---
+++ CCF_CSP/3.cpp
@@ -0,0 +1,15 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+//======================3.1门禁系统==================
+//#include<stdio.h>
+//int main() {
+//	int depsyada;
+//	int n; scanf("%d", &n);
+//	int num[1010] = { 0 }, temp;
+//	for (int i = 0; i < n; i++) {
+//		scanf("%d", &temp);
+//		printf("%d ", ++num[temp]);
+//	}
+//	return 0;
+//}
+
+
